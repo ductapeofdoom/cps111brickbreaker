8f919e9062e76d8926a40c10a9d033f991a188e9
Stephen-Sidwell <stevesidwell9@gmail.com>
2014-03-08 10:42:41 -0500

    Rearragned GameObject and Animation code.

--- CPS111BrickBreaker/gameobject.cpp
+++ CPS111BrickBreaker/gameobject.cpp
@@ -1,5 +1,6 @@
 #include "gameobject.h"
 #include "gamewindow.h"
+#include "gameworld.h"
 #include <cassert>
 
 //Units tests for ball collision detection
@@ -49,6 +50,22 @@ void collisionUnitTests(){
     assert(testBall->getX() == 478.5);
     assert(testBall->getY() == 91);
     //Test 6
+    testBall->setX(220);
+    testBall->setY(110);
+    testBall->updatePosition();
+    assert(testBall->getXHeading() == 1.5);
+    assert(testBall->getYHeading() == 1);
+    assert(testBall->getX() == 221.5);
+    assert(testBall->getY() == 111);
+    //Test 7
+    testBall->setX(80);
+    testBall->setY(100);
+    testBall->updatePosition();
+    assert(testBall->getXHeading() == -1.5);
+    assert(testBall->getYHeading() == 1);
+    assert(testBall->getX() == 78.5);
+    assert(testBall->getY() == 101);
+    //Test 8
     testBall->setX(80);
     testBall->setY(500);
     testBall->updatePosition();
@@ -56,4 +73,107 @@ void collisionUnitTests(){
     assert(testBall->getYHeading() == 0);
     assert(testBall->getX() == 150);
     assert(testBall->getY() == 80);
+
+}
+
+//Checks if the paddle has collided with the right side of the screen
+bool Paddle::checkCollisionRight()
+{
+    if (x >= 280){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//Checks to see if the paddle has collided with the left side of the screen
+bool Paddle::checkCollisionLeft()
+{
+    if (x <= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void Paddle::saveState()
+{
+
+}
+
+//Checks for the ball's collision and sets its headings accordingly. If the ball has gone off the bottom of the screen the ball is reset to the center of the paddle.
+void Ball::checkCollision()
+{
+    if (x <= 0){
+        xHeading = -1 * xHeading;
+    }
+    else if (y <= 0){
+        yHeading = -1 * yHeading;
+    }
+    else if (x >= 380) {
+        xHeading = -1 * xHeading;
+    }
+    else if (y >= 500){
+        yHeading = 0;
+        xHeading = 0;
+        x = paddle->getX() + 50;
+        y = paddle->getY() - 20;
+        initialPos = true;
+    }
+    else if ((x >= paddle->getX() - 20 && x <= paddle->getX() + 120) && (y >= paddle->getY() - 20 && y <= paddle->getY() + 20)){
+        //int testX = paddle->getX();
+        //int testY = paddle->getY();
+        if (((x+20 >= paddle->getX() - 20 && x >= paddle->getX() - 20 && x+20 <= paddle->getX() && x <= paddle->getX()) && y >= paddle->getY() - 20 && y <= paddle->getY() + 20) ||
+                ((x+20 <= paddle->getX() + 140 && x <= paddle->getX() + 140 && x+20 >= paddle->getX() + 120 && x >= paddle->getX() + 120) && y >= paddle->getY() - 20 && y <=paddle->getY() + 20)){
+            xHeading = -1 * xHeading;
+        }
+        else{
+            yHeading = -1 * yHeading;
+        }
+    }
+    else{
+        for(GameObject * obj: GameWorld::accessWorld().getObjects()){
+            Brick * brick = dynamic_cast<Brick*>(obj);
+            if (brick != NULL){
+                if(x >= brick->getX() - 20 && x <= brick->getX() + 40 && y >= brick->getY() - 20 && y <= brick->getY() + 20){
+                    //int testY = paddle->getY();
+                    if (((x+20 >= brick->getX() - 20 && x >= brick->getX() - 20 && x+20 <= brick->getX() && x <= brick->getX()) && y >= brick->getY() - 20 && y <= brick->getY() + 20) ||
+                            ((x+20 <= brick->getX() + 140 && x <= brick->getX() + 140 && x+20 >= brick->getX() + 120 && x >= brick->getX() + 120) && y >= brick->getY() - 20 && y <=brick->getY() + 20)){
+                        xHeading = -1 * xHeading;
+                    }
+                    else{
+                        yHeading = -1 * yHeading;
+                    }
+                }
+            }
+        }
+    }
+}
+
+//Updates the ball's x,y coordinates with its heading and sets it heading if the ball is sitting still.
+void Ball::updatePosition()
+{
+    if (initialPos){
+        if(paddle->getInitialLeft()){
+            yHeading = .2;
+            xHeading = -.5;
+            paddle->setInitialLeft(false);
+        }
+        else if(paddle->getInitialRight()){
+            yHeading = .2;
+            xHeading = .5;
+            paddle->setInitialRight(false);
+        }
+        initialPos = false;
+    }
+   checkCollision();
+   x = x + xHeading;
+   y = y + yHeading;
+}
+
+void Ball::saveState()
+{
+
 }
--- CPS111BrickBreaker/gamewindow.cpp
+++ CPS111BrickBreaker/gamewindow.cpp
@@ -10,6 +10,7 @@ GameWindow::GameWindow(QWidget *parent) :
     gameui(new Ui::GameWindow)
 {
     gameui->setupUi(this);
+    collisionUnitTests();
     cyclecount = 0;
     animTimer = new QTimer(this);
     animTimer->setInterval(1);
@@ -30,20 +31,30 @@ GameWindow::GameWindow(QWidget *parent) :
 //Animates <obj>. Designed specifically to move the ball from it currents coordinates to its next ones.
 void Animate(QObject * obj){
     GUIBall * ball = dynamic_cast<GUIBall*>(obj);
-    QPropertyAnimation * animation = new QPropertyAnimation(obj, "geometry");
-    animation->setDuration(1);
-    animation->setStartValue(QRect(ball->x(), ball->y(), 20, 20));
-    animation->setEndValue(QRect(ball->getBall()->getX(),ball->getBall()->getY(),20,20));
-    animation->start();
+    if (ball != NULL){
+        QPropertyAnimation * animation = new QPropertyAnimation(obj, "geometry");
+        animation->setDuration(1);
+        animation->setStartValue(QRect(ball->x(), ball->y(), 20, 20));
+        animation->setEndValue(QRect(ball->getBall()->getX(),ball->getBall()->getY(),20,20));
+        animation->start();
+    }
+    GUIPaddle * paddle = dynamic_cast<GUIPaddle*>(obj);
+    if (paddle != NULL){
+        QPropertyAnimation * animation = new QPropertyAnimation(obj, "geometry");
+        animation->setDuration(1);
+        animation->setStartValue(QRect(paddle->x(), paddle->y(), 120, 20));
+        animation->setEndValue(QRect(paddle->getPaddle()->getX(), paddle->getPaddle()->getY(), 120, 20));
+        animation->start();
+    }
 }
 
 //Slot implementaion for animTimerHit. Redraws the paddle and executes Animation every 10 cycles. It will also reset animation and stop the timer if the ball goes off the bottom.
 void GameWindow::animTimerHit(){
     GUIPaddle * paddle = dynamic_cast<GUIPaddle *>(gameui->wdGame->children().at(0));
-    paddle->setGeometry(QRect(paddle->getPaddle()->getX(), paddle->getPaddle()->getY(), 120, 20));
     GUIBall * ball = dynamic_cast<GUIBall *>(gameui->wdGame->children().at(1));
     ball->getBall()->updatePosition();
     if (cyclecount == 10){
+        Animate(paddle);
         Animate(ball);
         cyclecount = 0;
     }
@@ -55,9 +66,6 @@ void GameWindow::animTimerHit(){
         paddle->setInitialCommand(true);
         animTimer->stop();
     }
-    //ball->hide();
-    //ball->setGeometry(QRect(ball->getBall()->getX(), ball->getBall()->getY(), 20, 20));
-    //ball->show();
 }
 
 //Checks for the key press from <event> and sets the paddle's x coordinate and command state accordingly. InitialCommand is used for the first move of the game or when the ball has been reset.
@@ -85,91 +93,6 @@ void GUIPaddle::keyPressEvent(QKeyEvent *event)
              paddle->setX(paddle->getX() - 10);
             }
         }
-}
-
-//Checks if the paddle has collided with the right side of the screen
-bool Paddle::checkCollisionRight()
-{
-    if (x >= 280){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-//Checks to see if the paddle has collided with the left side of the screen
-bool Paddle::checkCollisionLeft()
-{
-    if (x <= 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-void Paddle::saveState()
-{
-
-}
-
-//Checks for the ball's collision and sets its headings accordingly. If the ball has gone off the bottom of the screen the ball is reset to the center of the paddle.
-void Ball::checkCollision()
-{
-    if (x <= 0){
-        xHeading = -1 * xHeading;
-    }
-    else if (y <= 0){
-        yHeading = -1 * yHeading;
-    }
-    else if (x >= 380) {
-        xHeading = -1 * xHeading;
-    }
-    else if (y >= 500){
-        yHeading = 0;
-        xHeading = 0;
-        x = paddle->getX() + 50;
-        y = paddle->getY() - 20;
-        initialPos = true;
-    }
-    else if ((x >= paddle->getX() - 20 && x <= paddle->getX() + 120) && (y >= paddle->getY() - 20 && y <= paddle->getY() + 20)){
-        //int testX = paddle->getX();
-        //int testY = paddle->getY();
-        if (((x+20 >= paddle->getX() - 20 && x >= paddle->getX() - 20 && x+20 <= paddle->getX() && x <= paddle->getX()) && y >= paddle->getY() - 20 && y <= paddle->getY() + 20) ||
-                ((x+20 <= paddle->getX() + 140 && x <= paddle->getX() + 140 && x+20 >= paddle->getX() + 120 && x >= paddle->getX() + 120) && y >= paddle->getY() - 20 && y <=paddle->getY() + 20)){
-            xHeading = -1 * xHeading;
-        }
-        else{
-            yHeading = -1 * yHeading;
-        }
-    }
-}
-
-//Updates the ball's x,y coordinates with its heading and sets it heading if the ball is sitting still.
-void Ball::updatePosition()
-{
-    if (initialPos){
-        if(paddle->getInitialLeft()){
-            yHeading = .2;
-            xHeading = -.5;
-            paddle->setInitialLeft(false);
-        }
-        else if(paddle->getInitialRight()){
-            yHeading = .2;
-            xHeading = .5;
-            paddle->setInitialRight(false);
-        }
-        initialPos = false;
-    }
-   checkCollision();
-   x = x + xHeading;
-   y = y + yHeading;
-}
-
-void Ball::saveState()
-{
-
 }
 
 //GameWindow destructor
